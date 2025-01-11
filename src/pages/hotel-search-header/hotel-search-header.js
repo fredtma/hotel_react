0d9a3cb0a504831307e5d810f35877db1b3251b4
frederick <frederick@synthesis.co.za>
2025-01-11 12:50:40 +0200

    Search Component debounc search

--- src/pages/hotel-search-header/hotel-search-header.js
+++ src/pages/hotel-search-header/hotel-search-header.js
@@ -1,6 +1,8 @@
 import { Form, InputGroup } from "react-bootstrap";
+import { debounce } from "../../services/debounce/debounce";
 
-export default function HotelSearchHeader() {
+export default function HotelSearchHeader({onSearch}) {
+  const searchInputHandler = debounce((e) => { onSearch(e.target.value);}, 500);
   return (
     <div>
       <InputGroup className="mb-3">
@@ -9,6 +11,7 @@ export default function HotelSearchHeader() {
           placeholder="Search"
           aria-label="Search"
           aria-describedby="basic-addon1"
+          onChange={searchInputHandler}
         />
       </InputGroup>
     </div>
