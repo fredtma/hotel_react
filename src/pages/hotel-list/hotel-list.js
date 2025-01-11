0c8487c0428ef057c7e2389b791291daa13f6a38
frederick <frederick@synthesis.co.za>
2025-01-11 12:57:06 +0200

    *** MVP: 4got no result serach

--- src/pages/hotel-list/hotel-list.js
+++ src/pages/hotel-list/hotel-list.js
@@ -2,10 +2,15 @@ import { Badge, Card, ListGroup } from "react-bootstrap";
 import { fetchOk } from "../../services/fetch/fetch.service";
 import { useEffect, useMemo, useState } from "react";
 
-export default function HotelList({ search = '' }) {
+export default function HotelList({ search = "" }) {
   const [items, setItems] = useState([]);
-  useEffect(() => { getItems().then(setItems.bind(null)); }, []);
-  const filterItems = useMemo(() => searchItems(items, search), [search, items]);
+  useEffect(() => {
+    getItems().then(setItems.bind(null));
+  }, []);
+  const filterItems = useMemo(
+    () => searchItems(items, search),
+    [search, items]
+  );
 
   return (
     <Card>
@@ -15,7 +20,7 @@ export default function HotelList({ search = '' }) {
           <ListGroup.Item
             key={item.id}
             as="li"
-            variant={i % 2 === 0 ? 'light' : 'primary'}
+            variant={i % 2 === 0 ? "light" : "primary"}
             className="d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
               <div className="fw-bold">{item.name}</div>
@@ -24,22 +29,27 @@ export default function HotelList({ search = '' }) {
             <Badge bg="primary">Bathrooms {item.bathrooms}</Badge>
           </ListGroup.Item>
         ))}
+        {filterItems.length === 0 && (
+          <ListGroup.Item as="li">No results</ListGroup.Item>
+        )}
       </ListGroup>
     </Card>
   );
 }
 
-
 function getItems() {
   return fetch("/assets/data/properties.json")
     .then(fetchOk)
     .catch((err) => console.error(err));
-};
+}
 
 function searchItems(items, search) {
-  return items.filter((item) => searchValue(item.name, search) || searchValue(item.description, search));
-};
+  return items.filter(
+    (item) =>
+      searchValue(item.name, search) || searchValue(item.description, search)
+  );
+}
 
 function searchValue(value, search) {
   return value.toLowerCase().includes(search.toLowerCase());
-};
+}
