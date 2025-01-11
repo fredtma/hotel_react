b4c0cbabd3ee957c82966921a90d46ef0baf562c
frederick <frederick@synthesis.co.za>
2025-01-11 12:51:03 +0200

    hotel search implemented

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HotelList from './pages/hotel-list/hotel-list';
 import HotelSearchHeader from './pages/hotel-search-header/hotel-search-header';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+  const [search, setSearch] = useState('');
   return (
     <Container>
       <Row>
@@ -13,8 +15,8 @@ function App() {
         </Col>
         <Col xs={12} md={10}>
           <h2>Hotel Listing</h2>
-          <HotelSearchHeader />
-          <HotelList />
+          <HotelSearchHeader onSearch={setSearch} />
+          <HotelList search={search} />
         </Col>
       </Row>
     </Container>
--- src/pages/hotel-list/hotel-list.js
+++ src/pages/hotel-list/hotel-list.js
@@ -1,18 +1,19 @@
 import { Badge, Card, ListGroup } from "react-bootstrap";
 import { fetchOk } from "../../services/fetch/fetch.service";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
-export default function HotelList() {
+export default function HotelList({ search = '' }) {
   const [items, setItems] = useState([]);
   useEffect(() => { getItems().then(setItems.bind(null)); }, []);
+  const filterItems = useMemo(() => searchItems(items, search), [search, items]);
 
   return (
     <Card>
       <Card.Header>Hotel List</Card.Header>
       <ListGroup as="ul">
-        {items.map((item, i) => (
+        {filterItems.map((item, i) => (
           <ListGroup.Item
-            key={i}
+            key={item.id}
             as="li"
             variant={i % 2 === 0 ? 'light' : 'primary'}
             className="d-flex justify-content-between align-items-start">
@@ -26,7 +27,6 @@ export default function HotelList() {
       </ListGroup>
     </Card>
   );
-
 }
 
 
@@ -34,4 +34,12 @@ function getItems() {
   return fetch("/assets/data/properties.json")
     .then(fetchOk)
     .catch((err) => console.error(err));
+};
+
+function searchItems(items, search) {
+  return items.filter((item) => searchValue(item.name, search) || searchValue(item.description, search));
+};
+
+function searchValue(value, search) {
+  return value.toLowerCase().includes(search.toLowerCase());
 };
