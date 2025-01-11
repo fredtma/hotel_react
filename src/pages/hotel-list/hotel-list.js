e39d05d5d39d4f90f9b532524e6da2307b757c0d
frederick <frederick@synthesis.co.za>
2025-01-11 12:02:59 +0200

    styleing og the hotel listing component

--- src/pages/hotel-list/hotel-list.js
+++ src/pages/hotel-list/hotel-list.js
@@ -1,4 +1,4 @@
-import { Card, ListGroup } from "react-bootstrap";
+import { Badge, Card, ListGroup } from "react-bootstrap";
 import { fetchOk } from "../../services/fetch/fetch.service";
 import { useEffect, useState } from "react";
 
@@ -9,10 +9,18 @@ export default function HotelList() {
   return (
     <Card>
       <Card.Header>Hotel List</Card.Header>
-      <ListGroup>
+      <ListGroup as="ul">
         {items.map((item, i) => (
-          <ListGroup.Item key={i}>
-            {item.name}
+          <ListGroup.Item
+            key={i}
+            as="li"
+            variant={i % 2 === 0 ? 'light' : 'primary'}
+            className="d-flex justify-content-between align-items-start">
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">{item.name}</div>
+              {item.description}
+            </div>
+            <Badge bg="primary">Bathrooms {item.bathrooms}</Badge>
           </ListGroup.Item>
         ))}
       </ListGroup>
